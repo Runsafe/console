f9501570fcdad06037910bd6c1cf07d2e6767148
Morten Nilsen <morten@runsafe.no>
2024-02-17 01:55:03 +0100

    test

--- server1/save.sh
+++ server1/save.sh
@@ -2,4 +2,7 @@
 #bukkit command 'dog speak Now jotting down everything that happened, for future reference.'
 #bukkit command 'say Forcing save, brace for lag!'
 #sleep 2
+pwd
+echo ~
+which bukkit
 bukkit command 'save-all'
